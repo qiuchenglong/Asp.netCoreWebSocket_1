3182c4c79f66612ccd4af99377914d86ee20e3fb
邱成龙 <qiucl@cityfun.com.cn>
2018-05-10 17:03:13 +0800

    socket官网demo测试

---
+++ WebApplication2/Startup.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(LogLevel.Debug);
+            loggerFactory.AddDebug(LogLevel.Debug);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+#if NoOptions
+            #region UseWebSockets
+            app.UseWebSockets();
+            #endregion
+#endif
+            //配置 WebSocket 中间件
+            #region UseWebSocketsOptions
+            //配置以下设置：
+            var webSocketOptions = new WebSocketOptions()
+            {
+                //向客户端发送“ping”的帧率，以确保代理保持连接处于打开状态。
+                KeepAliveInterval = TimeSpan.FromSeconds(120),
+                //用于接收数据的缓冲区的大小。只有高级用户才需要对其进行更改，以便根据数据大小调整性能。
+                ReceiveBufferSize = 4 * 1024
+            };
+            app.UseWebSockets(webSocketOptions);
+            #endregion
+
+            //接受 WebSocket 请求
+            #region AcceptWebSocket
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    //检查它是否是 WebSocket 请求
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        //接受 WebSocket 请求。
+                        //AcceptWebSocketAsync 方法将 TCP 连接升级到 WebSocket 连接，并提供 WebSocket 对象。使用 WebSocket 对象发送和接收消息。
+                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        await Echo(context, webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+
+            });
+            #endregion
+            app.UseFileServer();
+        }
+        // 发送和接收消息
+        #region Echo
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+
+            //代码接收消息并立即发回相同的消息。 一直在循环中执行此操作，直到客户端关闭连接。
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+        #endregion
+    }
+}
